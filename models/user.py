82a9b006bb6c268a8f5d24b4af3e69c481c758cd
Melody Barker <melodybarker2013@gmail.com>
2021-11-16 10:15:08 -0600

    get Users

---
+++ models/user.py
@@ -0,0 +1,16 @@
+class User():
+
+    def __init__(self, id, first_name, last_name, email,
+                bio, username, password, profile_image_url,
+                created_on, active, is_staff):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.bio = bio
+        self.username = username
+        self.password = password
+        self.profile_image_url = profile_image_url
+        self.created_on = created_on
+        self.active = active
+        self.is_staff = is_staff
