808017cac981afe469a41f4ffbeb48b77168d012
Blake Mcadams <blakemcadams@Blakes-MacBook-Air.local>
2021-11-20 13:14:46 -0600

    Updated Posts methods

---
+++ posts/request.py
@@ -0,0 +1,155 @@
+import sqlite3
+import json
+from models import Posts
+
+POSTS = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "category_id": 1,
+        "title": "Food Products",
+        "publication_date": "21 November 2021",
+        "image_url": "placeholder",
+        "content": '',
+        "approved": ''
+    },
+    {
+       "id": 2,
+        "user_id": 1,
+        "category_id": 2,
+        "title": "Food Products",
+        "publication_date": "21 November 2021",
+        "image_url": "image goes here",
+        "content": '',
+        "approved": '' 
+    },
+    {
+        "id": 3,
+        "user_id": 1,
+        "category_id": 2,
+        "title": "Fancy Example",
+        "publication_date": "20 November 2021",
+        "image_url": "placeholder",
+        "content": '',
+        "approved": ''
+    }
+]
+
+
+def get_all_posts():
+    with sqlite3.connect("./raremedia.db") as conn:
+        
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM posts p
+        """)
+        
+        posts = []
+        
+        dataset = db_cursor.fetchall()
+        
+        for row in dataset:
+            
+            post = Posts(row['id'], row['user_id'], row['category_id'], row['title'], row['publication_date'], row['image_url'], row['content'], row['approved'])
+            
+            posts.append(post.__dict__)
+    
+    return json.dumps(posts)
+    
+def get_single_post(id):
+    with sqlite3.connect("./raremedia.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        SELECT
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM posts p
+        WHERE p.id = ?
+        """, ( id, ))
+        
+        data = db_cursor.fetchone()
+        
+        post = Posts(data['id'], data['user_id'], data['category_id'], data['title'],
+                            data['publication_date'], data['image_url'],
+                            data['content'], data['approved'])
+        return json.dumps(post.__dict__)
+
+def create_post(new_post):
+    with sqlite3.connect("./raremedia.db") as conn:
+        db_cursor = conn.cursor()
+        
+        db_cursor.execute("""
+        INSERT INTO Posts
+            ( user_id, category_id, title, publication_date, image_url, content, approved )
+        VALUES
+            ( ?, ?, ?, ?, ?, ?, ?);
+        """, (new_post['user_id'], new_post['category_id'],
+              new_post['title'], new_post['publication_date'],
+              new_post['image_url'], new_post['content'], new_post['approved'] ))
+
+        id = db_cursor.lastrowid
+
+        new_post['id'] = id
+
+    return json.dumps(new_post)
+
+
+
+def delete_post(id):
+    with sqlite3.connect("./raremedia.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        DELETE FROM posts
+        WHERE id = ?
+        """, (id, ))
+        
+        
+        
+def update_post(id, new_post):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                user_id = ?,
+                category_id = ?,
+                title = ?,
+                publication_date = ?,
+                image_url = ?
+                content = ?
+                approved = ?
+        WHERE id = ?
+        """, (new_post['user_id'], new_post['category_id'],
+              new_post['title'], new_post['publication_date'],
+              new_post['image_url'], new_post['content'], new_post['approved'], id, ))
+
+        
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        
+        return False
+    else:
+        
+        return True
---
+++ request_handler.py
@@ -0,0 +1,136 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from posts import( 
+get_all_posts,
+get_single_post,
+create_post,
+delete_post,
+update_post
+)
+import json
+
+from posts.request import create_post, update_post
+
+class HandleRequests(BaseHTTPRequestHandler):
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        
+        if "?" in resource:
+            
+
+            param = resource.split("?")[1]  
+            resource = resource.split("?")[0]  
+            pair = param.split("=")  
+            key = pair[0]  
+            value = pair[1]  
+
+            return ( resource, key, value )
+
+        
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass 
+            except ValueError:
+                pass 
+
+            return (resource, id) 
+    
+
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+        
+    def do_GET(self):
+        self._set_headers(200)
+        response = {}
+        
+        parsed = self.parse_url(self.path)
+        
+        if len(parsed) == 2:
+            (resource, id) = parsed
+            
+            if resource == "posts":
+                if id is not None:
+                    response = f"{get_single_post(id)}"
+                    
+                else: 
+                    response = f"{get_all_posts()}"
+            
+                
+                # self.wfile.write(response.encode())
+                
+        self.wfile.write(f"{response}".encode())
+        
+    def do_POST(self):
+        self._set_headers(201)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        
+        post_body = json.loads(post_body)
+        
+        (resource, id) = self.parse_url(self.path)
+        
+        new_post = None 
+        
+        if resource == "posts":
+            new_post = create_post(post_body)
+            
+            self.wfile.write(f"{new_post}".encode())   
+    
+    
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+        
+        (resource, id) = self.parse_url(self.path)
+        
+        success = False
+        
+        if resource == "posts":
+            success = update_post(id, post_body)
+            
+        if success:
+            self._set_headers(204)
+        else:
+            self._set_headers(404)
+            
+        self.wfile.write("".encode())
+    
+    
+        
+    def do_DELETE(self):
+        self._set_headers(204)
+        
+        (resource, id) = self.parse_url(self.path)   
+        
+        if resource == "employees":
+            delete_post(id) 
+            
+        self.wfile.write("".encode())
+
+def main():
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
