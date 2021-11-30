5bf7e11504925b54fadd8e1f4dd43a99bbfdca6c
Brittany Garrett <brittanymgarrett@gmail.com>
2021-11-29 21:29:15 -0600

    Updated sql users and updated user model

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 
 class User():
-    def __init__(self, first_name, last_name,
+    def __init__(self, id, first_name, last_name,
                 email='', bio='', username='', password='', profile_image_url='',
-                created_on=None, active=True, is_staff=True, id=None):
+                created_on=None, active=True, is_staff=True):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
